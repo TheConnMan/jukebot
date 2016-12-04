aade362d2ad3dd41b8cbfa00976869fc3a55efb1
TheConnMan <bcconn2112@gmail.com>
2016-12-03 22:13:22 -0500

    Add YouTube API service

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # JukeBot
+Slack-Enabled Syncronized Music Listening
+
+## Setup
+### Google API Key for YouTube
+1. Go to https://console.developers.google.com
+1. Create a new Google Project named JukeBot
+1. Under **Create credentials** choose **API key** and use the API key for the environment variable below
+1. Click **Library** in the left panel
+1. Go to **YouTube Data API**
+1. Click **Enable** at the top
+
+## Environment Variables
+- **GOOGLE_API_KEY** - Google project API key
---
+++ api/services/YouTubeService.js
@@ -0,0 +1,54 @@
+var Promise = require('promise');
+var request = require('request');
+
+module.exports = {
+  getYouTubeVideo: getYouTubeVideo
+};
+
+function getYouTubeVideo(key, user) {
+  return new Promise(function(resolve, reject) {
+    request('https://www.googleapis.com/youtube/v3/videos?id=' + key + '&part=snippet,contentDetails&key=' + process.env.GOOGLE_API_KEY, function(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        parseYouTubeVideo(JSON.parse(body), user).exec(function(err, video) {
+          if (err) {
+            throw err;
+          }
+          resolve(video);
+        });
+      } else {
+        reject(error);
+      }
+    });
+  });
+}
+
+function parseYouTubeVideo(data, user) {
+  if (data.items.length != 1) {
+    throw 'Unexpected video count';
+  }
+  var item = data.items[0];
+  return Video.create({
+    key: item.id,
+    durationSeconds: parseDuration(item.contentDetails.duration),
+    user: user,
+    thumbnail: item.snippet.thumbnails.default.url
+  });
+}
+
+function parseDuration(duration) {
+  var seconds = 0;
+  duration.substring(2).match(/[1-9]+[A-Za-z]+/g).forEach(function(item) {
+    switch(item.slice(-1)) {
+      case 'H':
+        seconds += 3600 * parseInt(item.substring(0, item.length));
+        break;
+      case 'M':
+        seconds += 60 * parseInt(item.substring(0, item.length));
+        break;
+      case 'S':
+        seconds += parseInt(item.substring(0, item.length));
+        break;
+    }
+  });
+  return seconds;
+}
