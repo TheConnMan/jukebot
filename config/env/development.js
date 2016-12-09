ebf61347d282bbface4d5435e0c3cf89d6cd9891
TheConnMan <bcconn2112@gmail.com>
2016-12-09 16:15:16 -0500

    Add Slack Added and Playing notification environment variables

--- config/env/development.js
+++ config/env/development.js
@@ -1,5 +1,7 @@
 module.exports = {
 
   serverUrl: process.env.SERVER_URL || 'http://127.0.0.1:1337',
-  slackWebhook: process.env.SLACK_WEBHOOK
+  slackWebhook: process.env.SLACK_WEBHOOK,
+  slackSongAdded: process.env.SLACK_SONG_ADDED !== 'false',
+  slackSongPlaying: process.env.SLACK_SONG_PLAYING !== 'false'
 };
