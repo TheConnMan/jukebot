28e10a58ac80084822f43d5f377fe8095761bc24
TheConnMan <bcconn2112@gmail.com>
2016-12-12 19:55:33 -0500

    Show chat username again after 3 min of activity
    Closes #54

--- assets/js/components/chat.js
+++ assets/js/components/chat.js
@@ -5,7 +5,7 @@ function ChatController($scope, $http) {
   this.newChat = '';
 
   this.differentUser = function(index) {
-    return index === 0 || this.chats[index].username != this.chats[index - 1].username;
+    return index === 0 || this.chats[index].time - this.chats[index - 1].time > 3 * 60 * 1000 || this.chats[index].username != this.chats[index - 1].username;
   };
 
   this.sendChat = function() {
