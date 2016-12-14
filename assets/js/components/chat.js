5162a99c56cda5aeefc94a2cb27679b4ca5bca15
sirzach <zach.mcgonigal@gmail.com>
2016-12-13 22:57:28 -0500

    allow the user to toggle video notifications vs chat notifications

--- assets/js/components/chat.js
+++ assets/js/components/chat.js
@@ -1,20 +1,25 @@
-function ChatController($scope, $http, $notification) {
+function ChatController($scope, $http, $notification, $storage) {
   io.socket.get('/chat/subscribe', {});
 
   this.chats = [];
   this.newChat = '';
+  this.notifications = $storage.get('chat-notifications') === 'true' ||  !$storage.get('chat-notifications');
 
   this.differentUser = function(index) {
     return index === 0 || this.chats[index].time - this.chats[index - 1].time > 3 * 60 * 1000 || this.chats[index - 1].type === 'machine' || this.chats[index].username != this.chats[index - 1].username;
   };
 
+  this.toggleChat = function(newVal) {
+    $storage.set('chat-notifications', newVal);
+  };
+
   this.sendChat = function() {
     io.socket._raw.emit('chat', {
-       message: this.newChat,
-       username: this.username,
-       time: Date.now()
-     });
-     $('#chat-input input').val('');
+      message: this.newChat,
+      username: this.username,
+      time: Date.now()
+    });
+    $('#chat-input input').val('');
   };
 
   io.socket.on('chats', (c) => {
@@ -51,7 +56,6 @@ angular
   templateUrl: 'components/chat.html',
   controller: ChatController,
   bindings: {
-    username: '=',
-    notifications: '='
+    username: '='
   }
 });
