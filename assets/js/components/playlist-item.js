3a9150809635937fb69f79acf1146b8f0c37d1b0
sirzach <zach.mcgonigal@gmail.com>
2016-12-17 17:17:59 -0500

    refactored video cards to a playlistitem component

---
+++ assets/js/components/playlist-item.js
@@ -0,0 +1,49 @@
+function PlaylistItemController($scope, $http, $video, $storage) {
+  this.formatDuration = function() {
+    return $video.formatDuration(this.video.duration);
+  };
+
+  this.expectedPlayTime = function() {
+    return $video.expectedPlayTime(this.video);
+  };
+
+  this.skip = function() {
+    return $video.skip(this.username)
+      .then(() => this.scrollToBottom());
+  };
+
+  this.readd = function() {
+    return $video.addByKey(this.username, this.video.key)
+      .then(() => this.scrollToBottom());
+  };
+
+  this.remove = function() {
+    return $video.removePermanently(this.video.id)
+      .then(() => this.scrollToBottom());
+  };
+
+  this.likeVideo = function() {
+    $storage.likeVideo(this.video);
+    if (this.likesVideo(this.video.key)) {
+      $scope.$emit('likeVideo', {
+        video: this.video
+      });
+    }
+  };
+
+  this.likesVideo = function() {
+    return $storage.likesVideo(this.video.key);
+  };
+}
+
+angular
+.module('app')
+.component('playlistitem', {
+  templateUrl: 'components/playlist-item.html',
+  controller: PlaylistItemController,
+  bindings: {
+    video: '=',
+    username: '=',
+    scrollToBottom: '&'
+  }
+});
--- assets/js/components/playlist.js
+++ assets/js/components/playlist.js
@@ -1,5 +1,6 @@
 function PlaylistController($scope, $video, $storage) {
   this.notifications = $storage.get('notifications') === 'true' ||  !$storage.get('notifications');
+  this.activeTab = 'up-next';
 
   this.getVideos = function() {
     return $video.getVideos();
@@ -9,43 +10,7 @@ function PlaylistController($scope, $video, $storage) {
     $storage.set('notifications', newVal);
   };
 
-  this.skip = function() {
-    return $video.skip(this.username)
-      .then(() => scrollToBottom());
-  };
-
-  this.readd = function(key) {
-    return $video.addByKey(this.username, key)
-      .then(() => scrollToBottom());
-  };
-
-  this.remove = function(id) {
-    return $video.removePermanently(id)
-      .then(() => scrollToBottom());
-  };
-
-  this.likeVideo = function(video) {
-    $storage.likeVideo(video);
-    if (this.likesVideo(video.key)) {
-      $scope.$emit('likeVideo', {
-        video
-      });
-    }
-  };
-
-  this.likesVideo = function(key) {
-    return $storage.likesVideo(key);
-  };
-
-  this.formatDuration = function(duration) {
-    return $video.formatDuration(duration);
-  };
-
-  this.expectedPlayTime = function(video) {
-    return $video.expectedPlayTime(video);
-  };
-
-  function scrollToBottom() {
+  this.scrollToBottom = function() {
     let $list = $('#video-list');
     $list.animate({
      scrollTop: $list.prop('scrollHeight')
