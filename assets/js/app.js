a1233eec9dbd2beeff60a375c0d4b9650a1635b6
TheConnMan <bcconn2112@gmail.com>
2017-01-15 11:04:45 -0500

    Linkify links in chat
    Closes #146

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['notification', 'storage', 'video', 'angularMoment']);
+var app = angular.module('app', ['notification', 'storage', 'video', 'angularMoment', 'ngSanitize']);
 app.controller('controller', function($scope, $rootScope, $notification, $storage, $video, $timeout, $http, $log) {
   $('.ui.search')
     .search({
